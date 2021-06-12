4759aea3ced3837b5e2ef0220e11a8e1cb7777e6
Tu Nombre <you@example.com>
2021-06-12 16:55:06 -0600

    Funciona pdf pedido local sin imagen

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,11 +11,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        $this->app->singleton('mailer', function ($app) { 
-            $app->configure('services'); 
-            return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer'); 
-          });
-    }
+    //public function register()
+    //{
+    //    $this->app->singleton('mailer', function ($app) { 
+    //        $app->configure('services'); 
+    //        return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer'); 
+    //      });
+    //}
 }
